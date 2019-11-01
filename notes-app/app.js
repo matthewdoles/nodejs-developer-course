f8683c80a865de273fec280df172194440739896
Matthew Doles <matteddied@gmail.com>
2019-11-01 13:42:15 -0400

    3.3 - Importing Your Own Files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,5 @@
-const fs = require('fs');
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+const getNotes = require("./utils.js");
 
-// Challenge: Append a message to notes.txt
-//
-// 1. Use appendFileSync to append to files
-// 2. Run script
-// 3. Check notes.txt to verify appended text
+const msg = getNotes();
 
-fs.appendFileSync('notes.txt', 'Hello, my name is Matthew');
+console.log(msg);
---
+++ notes-app/export-basics-challenge.js
@@ -0,0 +1,11 @@
+// Challenge: Define and use a function in a new file
+//
+// 1. Create get notes function that returns "Your notes..."
+// 2. Export get notes function
+// 3. From app.js, load in and call the function and print message to console
+
+const getNotes = function() {
+    return "Your notes..."
+}
+
+module.exports = getNotes;
