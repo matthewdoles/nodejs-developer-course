5f799ed0a433b805cd1ed7482093c4c64051ea8e
Matthew Doles <matteddied@gmail.com>
2019-11-12 15:00:58 -0500

    7.6 - Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,18 +5,37 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
 
 // app.com
 // app.com/help
 // app.com/about
 
-// Challenge: Create two more HTML files
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Matthew Doles'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Matthew Doles'
+    })
+})
+
+// Challenge: Create a template for help page
 //
-// 1. Create a HTML page for about with 'About' title
-// 2. Create a HTML page for help with 'Help' title
-// 3. Remove the old rout handlers for both
-// 4. Visit both in the browser to test your work
+// 1. Setting a help template to render a help message to the screen
+// 2. Setup the help route and render the template with an example message
+// 3. Visit the route in the browser and see your help message print
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is helpful text.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
