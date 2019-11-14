9070f80e911ab4c210248ed1076dfd55ba6cf25f
Matthew Doles <matteddied@gmail.com>
2019-11-14 14:05:31 -0500

    8.7 - Wiring up the User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -8,19 +8,29 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#location')
+const messageTwo = document.querySelector('#temperature')
+const messageThree = document.querySelector('#summary')
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     
     const location = search.value
 
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    messageThree.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error
         }
         else {
             console.log(data)
+            messageOne.textContent = 'Location: ' + data.location
+            messageTwo.textContent = 'Temperature: ' + data.temperature + '° Fahrenheit'
+            messageThree.textContent = 'Outlook: ' + data.summary
         }
     })
 })
