b047663da525f27bdd30cdcce439dd5b262d6ddf
Matthew Doles <matteddied@gmail.com>
2019-11-23 12:53:39 -0500

    11.12 - Resource Reding Endpoints: Part II

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -45,6 +45,25 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((task) => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.listen(port, () => {
     console.log('Server is up at port', port)
 })
