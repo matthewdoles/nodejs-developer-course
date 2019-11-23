265b0756c9fe1c6b2c4debad5e89305c9f45d898
Matthew Doles <matteddied@gmail.com>
2019-11-23 12:46:08 -0500

    11.10 - Resource Reading Endpoints: Part I

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -17,6 +17,25 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then((user) => {
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
