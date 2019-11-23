ea087585770c7e4606d16ced029ac841c70450bb
Matthew Doles <matteddied@gmail.com>
2019-11-23 14:06:14 -0500

    11.15 - Async/Await

---
+++ task-app/async-await-basics.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        if (a < 0 || b < 0) {
+            return reject('Numbers must be non-negative')
+        }
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+const doWork = async () => {
+    const sum = await add(1, 99)
+    const sum2 = await add(sum, 150)
+    const sum3 = await add(sum2, -3)
+    return sum3
+}
+
+doWork().then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
