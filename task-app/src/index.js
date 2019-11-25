993442c3c7af8b5217ed5ae0f918b5e4a5d9461e
Matthew Doles <matteddied@gmail.com>
2019-11-25 10:29:11 -0500

    11.18 - Resource Updating Endpoints: Part I

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -42,6 +42,27 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValid = updates.every((update) =>  allowedUpdates.includes(update))
+
+    if (!isValid) {
+        return res.status(400).send({ error: 'invalid updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch(e) {
+        res.status(400).send(e)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     try {
