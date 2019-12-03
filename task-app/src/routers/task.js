a9d13f5528d8c246c45d082f98f4f43884ee627c
Matthew Doles <matteddied@gmail.com>
2019-12-03 13:37:04 -0500

    13.3 - Filtering Data

--- task-app/src/routers/task.js
+++ task-app/src/routers/task.js
@@ -18,9 +18,17 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+    const match  = {}
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+    
     try {
-        const tasks = await Task.find({ owner: req.user._id })
-        res.send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
+        res.send(req.user.tasks)
     } 
     catch(e) {
         res.status(500).send()
@@ -30,11 +38,11 @@ router.get('/tasks', auth, async (req, res) => {
 router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        await req.user.populate('tasks').execPopulate()
+        const task = await Task.find({_id, owner: req.user._id })
         if(!task) {
             return res.status(404).send()
         }
-        res.send(req.user.task)
+        res.send(task)
     }
     catch(e) {
         res.status(500).send()
