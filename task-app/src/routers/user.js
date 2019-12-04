6baac4c44dccfea3cd8a92f10dc06c2cfd59d8f2
Matthew Doles <matteddied@gmail.com>
2019-12-04 08:52:02 -0500

    14.5 - Handling Express Errors

--- task-app/src/routers/user.js
+++ task-app/src/routers/user.js
@@ -91,7 +91,7 @@ const upload = multer({
     },
     fileFilter(req, file, cb) {
         if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Image must be .jpg, .jpeg, or .png.'))
+            return cb(new Error('Must be image of .jpg, .jpeg, or .png format.'))
         }
         cb(undefined, true)
     }
@@ -99,6 +99,8 @@ const upload = multer({
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router
