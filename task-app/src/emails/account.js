74e79fe5199835a919b6a380b49ebd63a4db5d47
Matthew Doles <matteddied@gmail.com>
2019-12-05 09:02:54 -0500

    15.4 - Environment Variables

--- task-app/src/emails/account.js
+++ task-app/src/emails/account.js
@@ -1,8 +1,6 @@
 const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const sendgridAPIKey = 'temp'
-
-sgMail.setApiKey(sendgridAPIKey)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
