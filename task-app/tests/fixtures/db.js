d55737da1079740cac2e09cbef9fe66f11ca6209
Matthew Doles <matteddied@gmail.com>
2019-12-08 12:52:39 -0500

    16.14 - Testing with Task Data

--- task-app/tests/fixtures/db.js
+++ task-app/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 const User1Id = new mongoose.Types.ObjectId()
 const User1 = {
@@ -13,13 +14,54 @@ const User1 = {
     }]
 }
 
+const User2Id = new mongoose.Types.ObjectId()
+const User2 = {
+    _id: User2Id,
+    name: 'Test User 2',
+    email: 'testuser2@example.com',
+    password: 'waffle539!',
+    tokens: [{
+        token: jwt.sign({ _id: User2Id }, process.env.JWT_SECRET)
+    }]
+}
+
+const Task1 = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'First Task',
+    owner: User1Id
+}
+
+const Task2 = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Second Task',
+    completed: true,
+    owner: User1Id
+}
+
+const Task3 = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Third Task',
+    completed: false,
+    owner: User2Id
+}
+
 const setupDatabase = async () => {
     await User.deleteMany()
+    await Task.deleteMany()
     await new User(User1).save()
+    await new User(User2).save()
+    await new Task(Task1).save()
+    await new Task(Task2).save()
+    await new Task(Task3).save()
 }
 
 module.exports = {
     User1Id,
     User1,
+    User2Id,
+    User2,
+    Task1,
+    Task2,
+    Task3,
     setupDatabase
 }
--- task-app/tests/task.test.js
+++ task-app/tests/task.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest')
 const Task = require('../src/models/task')
 const app = require('../src/app')
-const { User1Id, User1, setupDatabase } = require('./fixtures/db')
+const { 
+    User1Id, 
+    User1, 
+    User2Id, 
+    User2, 
+    Task1,
+    Task2,
+    Task3,
+    setupDatabase 
+} = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
@@ -17,4 +26,24 @@ test('Should create task for user', async () => {
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toBe(false)
+})
+
+test('Should fetch tasks for authenticated user', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${User1.tokens[0].token}`)
+        .send()
+        .expect(200)
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should prevent user deleting tasks they do not own', async () => {
+    const response = await request(app)
+        .delete(`/tasks/${Task1._id}`)
+        .set('Authorization', `Bearer ${User2.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    const task = Task.findById(Task1._id)
+    expect(task).not.toBeNull()
 })
