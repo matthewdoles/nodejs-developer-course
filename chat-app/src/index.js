be710ee9ea5086ef7b1d35891b5e426c72952f2f
Matthew Doles <matteddied@gmail.com>
2019-12-09 13:24:32 -0500

    17.6 - Socket.io Events Challenge

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,14 +12,11 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
-    socket.emit('countUpdated', count)
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
 })
 
