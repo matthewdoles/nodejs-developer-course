f6e4c970b934e0f41b3d56a893bbcd3f35118e8e
Matthew Doles <matteddied@gmail.com>
2019-12-10 14:41:14 -0500

    17.21 - Sending Messages to Rooms

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -22,29 +22,31 @@ io.on('connection', (socket) => {
             return callback(error)
         }
         socket.join(user.room)
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
         callback()
     })
     
     socket.on('sendMessage', (message, callback) => {
+        const user =  getUser(socket.id)
         const filter = new Filter()
         if (filter.isProfane(message)) {
             return callback('Message not sent, profanity found.')
         }
-        io.emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(coords))
+        const user =  getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, coords))
         callback()
     })
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
         }
     })
 })
